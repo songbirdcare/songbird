85e4514f4430640cf673089534197188ef638bfd
Nasr Maswood <nasr@songbirdcare.com>
2022-12-09 20:40:39 -0500

    get a dummy api server going

--- typescript/packages/api/src/index.ts
+++ typescript/packages/api/src/index.ts
@@ -1,1 +1,14 @@
-export const foo = 1;
+import * as http from "http";
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
