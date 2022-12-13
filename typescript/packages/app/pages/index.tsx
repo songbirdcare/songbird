87a7cbddc59a8ed881b5a0a6850ab3135ab0d0c9
Nasr Maswood <nasr@songbirdcare.com>
2022-12-13 15:11:08 -0500

    remove TS errors

--- typescript/packages/app/pages/index.tsx
+++ typescript/packages/app/pages/index.tsx
@@ -3,14 +3,14 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles["container"]}>
       <Head>
         <title>Songbird Therapy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Songbird</h1>
+      <main className={styles["main"]}>
+        <h1 className={styles["title"]}>Welcome to Songbird</h1>
       </main>
     </div>
   );
