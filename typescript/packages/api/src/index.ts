5c6a6dfc81da445f9024157e0fcff8bf4bac87f5
Nasr Maswood <nasr@songbirdcare.com>
2022-12-13 15:59:59 -0500

    add gcloudignore

--- typescript/packages/api/src/index.ts
+++ typescript/packages/api/src/index.ts
@@ -16,7 +16,15 @@ const QUOTES = [
 http
   .createServer((_, res) => {
     res.statusCode = 201;
-    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Type", "application/plain");
+
+    // #TODO deal w/ CORS later
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
     res.end(QUOTES[Math.floor(Math.random() * QUOTES.length)]);
   })
   .listen(SETTINGS.port, SETTINGS.host, () => {
