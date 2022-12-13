40590b2e7f84b3a911fafba6b39102983278af69
Nasr Maswood <nasr@songbirdcare.com>
2022-12-13 18:07:22 -0500

    add more quotes

--- typescript/packages/api/src/index.ts
+++ typescript/packages/api/src/index.ts
@@ -11,6 +11,27 @@ const QUOTES = [
   "There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy.",
   "Something is rotten in the state of Denmark.",
   "Alas, poor Yorick, I knew him Horatio.",
+
+  "By the pricking of my thumbs, something wicked this way comes.",
+  "Infirm of purpose!",
+  "More is thy due than more than all can pay.",
+  "I have bought golden opinions from all sorts of people.",
+  "The attempt and not the deed confounds us.",
+  "Out damned spot!",
+  "Will all great Neptune's ocean wash this blood clean from my hand?",
+  "",
+  "The bell invites me. Hear it not, Duncan; for it is a knell that summons thee to heaven or to hell.",
+  "There are daggers in men’s smiles.g",
+  "There's no art to find the mind's construction in the face.",
+  "That which hath made them drunk hath made me bold, what hath quenched them hath given me fire.",
+  "Methought I heard a voice cry, ‘Sleep no more! Macbeth does murder sleep’g",
+  "Look like th’ innocent flower, but be the serpent under ‘t.g",
+  "It was the owl that shrieked, the fatal bellman, which gives the stern'st good-night.",
+  "Double, double, toil and trouble, fire burn and cauldron bubbleg",
+  "He died as one that had been studied in his death to throw away the dearest thing he owed, as 't were a careless trifle.",
+  "Screw your courage to the sticking place.g",
+  "All the perfumes of Arabia will not sweeten this little hand.g",
+  "What's done cannot be undone.",
 ];
 
 http
