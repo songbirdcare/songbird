62314c3a2f2252ca868f4a8bf428a22dfb79eb8f
Nasr Maswood <nasr@songbirdcare.com>
2022-12-13 16:20:27 -0500

    comment stuff out

--- typescript/packages/app/pages/index.tsx
+++ typescript/packages/app/pages/index.tsx
@@ -2,18 +2,18 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 
-const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT as string;
+//const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT as string;
 
 export default function Home() {
-  const [copy, setCopy] = useState("");
+  const [copy] = useState("");
 
-  useEffect(() => {
-    async function f() {
-      const res = await fetch(API_ENDPOINT);
-      setCopy(await res.text());
-    }
-    f();
-  }, []);
+  //useEffect(() => {
+  //async function f() {
+  //const res = await fetch(API_ENDPOINT);
+  //setCopy(await res.text());
+  //}
+  //f();
+  //}, []);
 
   return (
     <div className={styles.container}>
