a882c253554b5bf5a8b00491194a039518b01455
Nasr Maswood <nasr@songbirdcare.com>
2022-12-15 14:54:50 -0500

    start work on first page

--- typescript/packages/app/pages/index.tsx
+++ typescript/packages/app/pages/index.tsx
@@ -1,46 +1,68 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 
+import * as React from "react";
+
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default withPageAuthRequired(function Home() {
   const { user } = useUser();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Songbird Therapy</title>
         <meta name="description" content="Innovative care" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Box>
+        <AppBar
+          position="static"
+          sx={{ bgcolor: "white", paddingLeft: "0", paddingRight: "0" }}
+        >
+          <Toolbar
+            disableGutters
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(4, 1fr)",
+              gridTemplateRows: "1fr",
+              gridColumnGap: "0px",
+              gridrowGap: "0px",
+            }}
+          >
+            <Box
+              gridArea="1 / 2 / 2 / 4"
+              display="flex"
+              justifyContent="center"
+            >
+              <Image
+                src="/songbird-logo.svg"
+                alt="me"
+                width="128"
+                height="64"
+              />
+            </Box>
 
-      {user ? (
-        <a href="/api/auth/logout">Logout</a>
-      ) : (
-        <a href="/api/auth/login">Login</a>
-      )}
-
-      <Profile />
+            <Box
+              gridArea="1 / 4 / 2 / 5"
+              display="flex"
+              justifyContent="flex-end"
+              marginRight={2}
+            >
+              {user && <Typography color={"black"}>{user.name}</Typography>}
+            </Box>
+          </Toolbar>
+          <Box></Box>
+        </AppBar>
+      </Box>
     </div>
   );
 });
-
-const Profile: React.FunctionComponent = () => {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-  if (!user) {
-    return null;
-  }
-
-  return (
-    user && (
-      <div>
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
-  );
-};
