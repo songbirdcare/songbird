ca9c3ffd4dee4ed6fc7ef579c954bef07c0f835e
Nasr Maswood <nasr@songbirdcare.com>
2022-12-14 21:47:58 -0500

    refactor fastify for express

--- typescript/packages/api/src/index.ts
+++ typescript/packages/api/src/index.ts
@@ -1,64 +1,30 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import cors from "cors";
 import { SETTINGS } from "./settings";
-import Fastify from "fastify";
-import { CLIENT } from "./redis/redis";
-import { fastifyLogSettings } from "./logger";
 
-const fastify = Fastify({
-  logger: fastifyLogSettings(SETTINGS.logFormat),
-});
+import { CLIENT } from "./redis/redis";
+
 console.log("Booting application");
 
 async function start() {
-  await CLIENT.connect();
+  const app = express();
+  const router = express.Router();
+  app.use(express.json());
+  app.use(cors({ origin: "*" }));
 
-  // TODO enable CORS later
-  fastify.addHook("onRequest", (_, reply, done) => {
-    reply.header("Access-Control-Allow-Origin", "*");
-    reply.header("Access-Control-Allow-Headers", "*");
-    reply.header("Access-Control-Allow-Methods", "*");
-    done();
-  });
-
-  fastify.get("/", async function (_, reply) {
+  app.get("/", async (_: express.Request, res: express.Response) => {
     await CLIENT.incr("count");
     const count = Number((await CLIENT.get("count")) ?? 0);
-    reply.send({ count });
+    res.json({ count });
   });
 
-  fastify.register(
-    async function (fastify) {
-      fastify.get("/healthz", async function (_, reply) {
-        reply.send("ok");
-        return reply;
-      });
+  app.use("/api/v1", router);
 
-      fastify.get("/auth/callback", async function (_, reply) {
-        reply.send("callback");
-        return reply;
-      });
+  await CLIENT.connect();
 
-      fastify.get("/auth/logout", async function (_, reply) {
-        reply.send("logout");
-        return reply;
-      });
-    },
-
-    {
-      prefix: "/api/v1",
-    }
-  );
-
-  fastify.listen(
-    { port: SETTINGS.port, host: SETTINGS.host },
-    function (err, _) {
-      if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-      }
-    }
-  );
+  app.listen(SETTINGS.port, SETTINGS.host);
 }
 
 start();
