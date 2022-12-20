58063759f7aa5cc42660f509511180400fc4134d
Nasr Maswood <nasr@songbirdcare.com>
2022-12-20 17:27:36 -0500

    provide a seperate sign up flow

--- typescript/packages/app/pages/api/auth/[...auth0].ts
+++ typescript/packages/app/pages/api/auth/[...auth0].ts
@@ -1,3 +1,24 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default handleAuth();
+export default handleAuth({
+  async signup(req: NextApiRequest, res: NextApiResponse) {
+    const email = getEmail(req.query);
+    handleLogin(req, res, {
+      authorizationParams: {
+        screen_hint: "signup",
+        login_hint: email,
+      },
+    });
+  },
+});
+
+function getEmail(query: NextApiRequest["query"]): string {
+  const email = query.email;
+  if (Array.isArray(email)) {
+    return email[0] ?? "";
+  } else if (!email) {
+    return "";
+  }
+  return email;
+}
