0baca13289a8361f57b1d34c5293f82b06d7e4ea
Nasr Maswood <nasr@songbirdcare.com>
2022-12-22 13:06:43 -0500

    add fonts to mui theme

--- typescript/packages/app/src/style/theme.tsx
+++ typescript/packages/app/src/style/theme.tsx
@@ -1,14 +1,37 @@
 import { createTheme } from "@mui/material/styles";
+import { Castoro, IBM_Plex_Sans } from "@next/font/google";
 
 import { SONG_BIRD_GREEN } from "./colors";
 
+const castoro = Castoro({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const ibmPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  preload: true,
+});
+
+const HEADER_FONT = castoro.style.fontFamily;
+
+export const NON_HEADER_FONT = ibmPlex.style.fontFamily;
+
 export const THEME = createTheme({
   typography: {
-    fontFamily: `"Circular-Loom", sans-serif`,
-    fontSize: 14,
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
+    h1: { fontFamily: HEADER_FONT },
+    h2: { fontFamily: HEADER_FONT },
+    h3: { fontFamily: HEADER_FONT },
+    h4: { fontFamily: HEADER_FONT },
+    h5: { fontFamily: HEADER_FONT },
+    h6: { fontFamily: HEADER_FONT },
+    subtitle1: { fontFamily: NON_HEADER_FONT },
+    body1: { fontFamily: NON_HEADER_FONT },
+    body2: { fontFamily: NON_HEADER_FONT },
+    button: { fontFamily: NON_HEADER_FONT },
+    caption: { fontFamily: NON_HEADER_FONT },
+    overline: { fontFamily: NON_HEADER_FONT },
   },
   palette: {
     primary: {
