20cee776a120fc877f02bf01812b544640074615
Nasr Maswood <nasr@songbirdcare.com>
2022-12-23 13:51:35 -0500

    fix verify email stuff

--- typescript/packages/app/src/verify-email.tsx
+++ typescript/packages/app/src/verify-email.tsx
@@ -14,17 +14,12 @@ export const VerifyEmail: React.FC = () => {
       <Box
         display="flex"
         height="100%"
-        flexDirection="column"
         alignItems="center"
         justifyContent="center"
         width="725px"
       >
         <Typography variant="h5" align="center">
           Please verify your email
-        </Typography>
-
-        <Typography variant="h5" align="center">
-          Please sign out and back in again once your account has been verified
         </Typography>
       </Box>
     </Box>
