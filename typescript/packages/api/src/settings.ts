8fcd1de9b91162a050e22853a8ab653a7e8021f2
Nasr Maswood <nasr@songbirdcare.com>
2023-01-03 18:14:27 -0500

    add another issuer

--- typescript/packages/api/src/settings.ts
+++ typescript/packages/api/src/settings.ts
@@ -7,7 +7,7 @@ const Settings = z.object({
     issuerBaseUrl: z.string(),
     jwksUri: z.string(),
     audience: z.string(),
-    issuer: z.string(),
+    issuer: z.array(z.string()),
     machineClientId: z.string(),
     machineSecret: z.string(),
     domain: z.string(),
@@ -23,6 +23,14 @@ const Settings = z.object({
 type Settings = z.infer<typeof Settings>;
 
 const domain = process.env["AUTH0_DOMAIN"];
+const additionalIssuer = process.env["AUTH0_ADDITIONAL_ISSUER"];
+const issuer: string[] = [`https://${domain}/`];
+if (typeof additionalIssuer === "string") {
+  issuer.push(additionalIssuer);
+}
+
+console.log(`Issuer: ${JSON.stringify(issuer)}`);
+
 export const SETTINGS = Settings.parse({
   host: process.env["HOST"] ?? "0.0.0.0",
   port: Number(process.env["PORT"] ?? "8080"),
@@ -30,7 +38,7 @@ export const SETTINGS = Settings.parse({
     issuerBaseUrl: `https://${domain}`,
     jwksUri: `https://${domain}/.well-known/jwks.json`,
     audience: process.env["AUTH0_AUDIENCE"],
-    issuer: `https://${domain}/`,
+    issuer,
     machineClientId: process.env["AUTH0_MACHINE_CLIENT_ID"],
     machineSecret: process.env["AUTH0_MACHINE_SECRET"],
     domain,
