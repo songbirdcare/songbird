76cf792603676422eeafb3ed17ff8d4eb7c108c8
Nasr Maswood <nasr@songbirdcare.com>
2023-01-06 14:00:36 -0500

    hard code form to test

--- typescript/packages/app/src/render-workflow.tsx
+++ typescript/packages/app/src/render-workflow.tsx
@@ -1,6 +1,8 @@
 import type { WorkflowModel } from "@songbird/precedent-iso";
 import type { Stage } from "@songbird/precedent-iso";
 import { assertNever } from "@songbird/precedent-iso";
+import EmbedFlow from "@formsort/react-embed";
+
 import * as React from "react";
 
 export const RenderWorkflow: React.FC<{ workflow: WorkflowModel }> = ({
@@ -20,7 +22,19 @@ export const RenderStage: React.FC<{ stage: Stage }> = ({ stage }) => {
   switch (stage.type) {
     case "create_account":
     case "check_insurance_coverage":
-      return null;
+      return (
+        <EmbedFlow
+          clientLabel="KBSuFeF9MN"
+          flowLabel="nasr-sandbox"
+          variantLabel="main"
+          embedConfig={{
+            style: {
+              width: "100%",
+              height: "100%",
+            },
+          }}
+        />
+      );
     case "submit_records":
     case "commitment_to_care":
       throw new Error("not implemented");
