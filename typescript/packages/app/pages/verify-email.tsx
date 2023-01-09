0f9fe05050506957369eb8f2a2951738f66123f5
Nasr Maswood <nasr@songbirdcare.com>
2023-01-09 13:48:45 -0500

    sort stuff

--- typescript/packages/app/pages/verify-email.tsx
+++ typescript/packages/app/pages/verify-email.tsx
@@ -3,11 +3,10 @@ import LinearProgress from "@mui/material/LinearProgress";
 import { useRouter } from "next/router";
 import * as React from "react";
 
-import { useFetchUser } from "../src/hooks/use-fetch-user";
-import { VerifyEmail } from "../src/verify-email";
-
 import { AppBar } from "../src/app-bar/app-bar";
 import { BodyContainer } from "../src/body-container";
+import { useFetchUser } from "../src/hooks/use-fetch-user";
+import { VerifyEmail } from "../src/verify-email";
 
 const Home: React.FC = () => {
   const { data: user, isLoading: userIsLoading } = useFetchUser();
