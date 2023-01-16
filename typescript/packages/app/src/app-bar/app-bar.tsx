5cf51d404e6a5d4cc8d6ed6ae99572633c3b7984
Nasr Maswood <10689975+nmaswood@users.noreply.github.com>
2023-01-15 19:32:38 -0500

    Bunch of random changes (#45)

--- typescript/packages/app/src/app-bar/app-bar.tsx
+++ typescript/packages/app/src/app-bar/app-bar.tsx
@@ -17,6 +17,7 @@ import { useFetchUser } from "../hooks/use-fetch-user";
 import { useFetchWorkflow } from "../hooks/use-fetch-workflow";
 import { SETTINGS } from "../settings";
 import { SONG_BIRD_GREEN } from "../style/colors";
+import styles from "./app-bar.module.css";
 
 export const AppBar: React.FC = () => {
   const { data: user } = useFetchUser();
@@ -37,30 +38,25 @@ export const AppBarBody: React.FC<{
         zIndex: 100,
       }}
     >
-      <Toolbar
-        disableGutters
-        sx={{
-          display: "grid",
-          gridTemplateColumns: "repeat(4, 1fr)",
-          gridTemplateRows: "1fr",
-          gridColumnGap: "0px",
-          gridrowGap: "0px",
-        }}
-      >
-        <Box gridArea="1 / 2 / 2 / 4" display="flex" justifyContent="center">
+      <Toolbar disableGutters className={styles["toolbar"] as string}>
+        <Box
+          className={styles["logo"] as string}
+          display="flex"
+          justifyContent="center"
+        >
           <Link href="/" underline="none">
             <Image src="/songbird-logo.svg" alt="me" width="128" height="64" />
           </Link>
         </Box>
 
         <Box
-          gridArea="1 / 4 / 2 / 5"
+          className={styles["right-side"] as string}
           display="flex"
           justifyContent="flex-end"
           marginRight={2}
           alignItems="center"
         >
-          <Box>
+          <Box className={styles["display-name"] as string}>
             {displayName && (
               <Typography color={"black"}>{displayName}</Typography>
             )}
