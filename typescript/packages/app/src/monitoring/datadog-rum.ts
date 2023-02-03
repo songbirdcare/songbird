01af0c23eb0cde3ad8149f470610ad6c4a6b314e
Nasr Maswood <nasr@songbirdcare.com>
2023-02-03 13:55:26 -0500

    baz

--- typescript/packages/app/src/monitoring/datadog-rum.ts
+++ typescript/packages/app/src/monitoring/datadog-rum.ts
@@ -13,18 +13,17 @@ export function initForRum({ id, email }: Args) {
     service: SETTINGS.datadog.service,
     env: SETTINGS.datadog.env,
 
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
     sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
+    sessionReplaySampleRate: 100,
     trackUserInteractions: true,
     trackResources: true,
     trackLongTasks: true,
+    trackFrustrations: true,
     defaultPrivacyLevel: "mask-user-input",
   });
 
   datadogRum.startSessionReplayRecording();
-
+  console.log("dd rum initialized");
   datadogRum.setUser({
     id,
     email,
