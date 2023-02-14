a6ce8d17bd55a14d59b0a8df3169d57981a7ea03
Nasr Maswood <nasr@songbirdcare.com>
2023-02-14 13:23:18 -0500

    redirect to thank you

--- typescript/packages/app/src/hooks/use-redirect-if-not-eligible.ts
+++ typescript/packages/app/src/hooks/use-redirect-if-not-eligible.ts
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import React from "react";
 
-import { TRACKER } from "../track";
 import { useFetchChild } from "./use-fetch-child";
 
 export const useRedirectIfNotEligible = () => {
@@ -12,11 +11,8 @@ export const useRedirectIfNotEligible = () => {
   const disqualified = data?.qualified?.type === "disqualified";
 
   React.useEffect(() => {
-    // TODO wait for 2 days of data to come in
-    // then start redirecting to thank-you page
     if (disqualified) {
-      TRACKER.track("redirect_to_disqualified");
-      console.warn("redirect_to_disqualified");
+      router.push("/thank-you");
     }
   }, [router, disqualified]);
 
