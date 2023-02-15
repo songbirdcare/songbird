59c492b459e71ab2153796b71113c9b225324c28
Nasr Maswood <10689975+nmaswood@users.noreply.github.com>
2023-02-15 14:47:15 -0500

    Begin typing workflow (#108)
    * make workflow slug required
    
    * parse task from workflow

--- typescript/packages/api/src/services/workflow/create-initial-workflow.ts
+++ typescript/packages/api/src/services/workflow/create-initial-workflow.ts
@@ -1,56 +1,74 @@
-import type { OnboardingStage, WorkflowSlug } from "@songbird/precedent-iso";
+import {
+  assertNever,
+  OnboardingStage,
+  WorkflowSlug,
+} from "@songbird/precedent-iso";
 import { randomUUID } from "crypto";
 
 export const CURRENT_VERSION = 1 as const;
 export const INITIAL_SLUG: WorkflowSlug = "onboarding";
 
-export function createInitialStages(): OnboardingStage[] {
-  return [
-    {
-      id: randomUUID(),
-      type: "create_account",
-      blockingTasks: [
-        {
-          id: randomUUID(),
-          status: "pending",
-          type: "schedule",
-        },
-      ],
-    },
-    {
-      id: randomUUID(),
-      type: "check_insurance_coverage",
-      blockingTasks: [
-        {
-          id: randomUUID(),
-          status: "pending",
-          type: "form",
-          slug: "check_insurance_coverage",
-        },
-      ],
-    },
-    {
-      id: randomUUID(),
-      type: "submit_records",
-      blockingTasks: [
-        {
-          id: randomUUID(),
-          status: "pending",
-          type: "form",
-          slug: "submit_records",
-        },
-      ],
-    },
-    {
-      id: randomUUID(),
-      type: "commitment_to_care",
-      blockingTasks: [
-        {
-          id: randomUUID(),
-          status: "pending",
-          type: "signature",
-        },
-      ],
-    },
-  ];
+export class CreateInitialWorkflow {
+  static forSlug(workflowSlug: WorkflowSlug): OnboardingStage[] {
+    switch (workflowSlug) {
+      case "onboarding":
+        return CreateInitialWorkflow.forOnboarding();
+      case "care_plan":
+      case "care_team":
+        throw new Error("not implemented");
+      default:
+        assertNever(workflowSlug);
+    }
+  }
+
+  static forOnboarding(): OnboardingStage[] {
+    return [
+      {
+        id: randomUUID(),
+        type: "create_account",
+        blockingTasks: [
+          {
+            id: randomUUID(),
+            status: "pending",
+            type: "schedule",
+          },
+        ],
+      },
+      {
+        id: randomUUID(),
+        type: "check_insurance_coverage",
+        blockingTasks: [
+          {
+            id: randomUUID(),
+            status: "pending",
+            type: "form",
+            slug: "check_insurance_coverage",
+          },
+        ],
+      },
+      {
+        id: randomUUID(),
+        type: "submit_records",
+        blockingTasks: [
+          {
+            id: randomUUID(),
+            status: "pending",
+            type: "form",
+            slug: "submit_records",
+          },
+        ],
+      },
+      {
+        id: randomUUID(),
+        type: "commitment_to_care",
+        blockingTasks: [
+          {
+            id: randomUUID(),
+            status: "pending",
+            type: "signature",
+          },
+        ],
+      },
+    ];
+  }
 }
