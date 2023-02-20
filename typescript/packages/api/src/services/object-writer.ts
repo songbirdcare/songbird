185448e530480bf69a4b92b83d892d5009a0af2d
Nasr Maswood <nasr@songbirdcare.com>
2023-02-19 21:50:27 -0500

    object writer

--- typescript/packages/api/src/services/object-writer.ts
+++ typescript/packages/api/src/services/object-writer.ts
@@ -1,4 +1,11 @@
+import { Bucket, Storage } from "@google-cloud/storage";
 import { promises } from "fs";
+
+export class GCSObjectWriter implements ObjectWriter {
+  writeFile(_: string, __: string): Promise<void> {
+    throw new Error("not implemented");
+  }
+}
 
 export class LocalObjectWriter implements ObjectWriter {
   writeFile(path: string, data: string): Promise<void> {
