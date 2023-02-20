9f11512e40105c848658637137e73b09b97a0571
Nasr Maswood <nasr@songbirdcare.com>
2023-02-19 21:44:47 -0500

    time the job

--- typescript/packages/api/src/job-runner/main.ts
+++ typescript/packages/api/src/job-runner/main.ts
@@ -9,19 +9,22 @@ import { GreenhouseServiceImpl } from "../services/greenhouse/greenhouse-service
 import { JOB_RUNNER_SETTINGS } from "./job-runner-settings";
 
 async function main() {
+  const startTime = performance.now();
+
+  console.log(`Starting ${JOB_RUNNER_SETTINGS.jobType} job`);
+
   switch (JOB_RUNNER_SETTINGS.jobType) {
     case "greenhouse":
-      {
-        console.log("Preparing greenhouse report");
-
-        const impl = new GreenhouseServiceImpl(JOB_RUNNER_SETTINGS.greenhouse);
-
-        await impl.writeReport();
-      }
+      await new GreenhouseServiceImpl(
+        JOB_RUNNER_SETTINGS.greenhouse
+      ).writeReport();
       break;
     default:
       assertNever(JOB_RUNNER_SETTINGS.jobType);
   }
+
+  const endTime = performance.now();
+  console.log(`Job took  ${endTime - startTime} milliseconds`);
 }
 
 main().then(() => console.log("job done!"));
