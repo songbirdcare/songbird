e344d8aecbe8420124810dafc11ea9983970f3b8
Nasr Maswood <nasr@songbirdcare.com>
2023-02-22 15:52:50 -0500

    keep going

--- typescript/packages/app/src/admin/admin-for-user.tsx
+++ typescript/packages/app/src/admin/admin-for-user.tsx
@@ -1,4 +1,12 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 import type { Child, Provider, UserModel } from "@songbird/precedent-iso";
 import React from "react";
 
@@ -11,24 +19,38 @@ export const AdminForUser: React.FC<{
     child.assessorId
   );
   return (
-    <FormControl fullWidth>
-      <InputLabel id="accessor-bcba">Accessor BCBA</InputLabel>
-      <Select
-        labelId="accessor-bcba"
-        id="accessor-bcba"
-        value={accessorId ?? ""}
-        label="Accessor BCBA"
-        onChange={(e) => {
-          setAccessorId(e.target.value === "" ? undefined : e.target.value);
-        }}
-      >
-        <MenuItem value={""}>No BCBA Accessor</MenuItem>
-        {providers.map((provider) => (
-          <MenuItem key={provider.id} value={provider.id}>
-            {provider.firstName} {provider.lastName}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <Box
+      paddingX={2}
+      paddingY={3}
+      width="750px"
+      height="100%"
+      flexDirection={"column"}
+      gap={4}
+    >
+      <Box marginBottom={2}>
+        <Typography>{user.email}</Typography>
+      </Box>
+      <FormControl fullWidth>
+        <InputLabel id="accessor-bcba">Accessor BCBA</InputLabel>
+        <Select
+          labelId="accessor-bcba"
+          id="accessor-bcba"
+          value={accessorId ?? ""}
+          label="Accessor BCBA"
+          onChange={(e) => {
+            setAccessorId(e.target.value === "" ? undefined : e.target.value);
+          }}
+        >
+          <MenuItem value={""}>No BCBA Selected</MenuItem>
+          {providers.map((provider) => (
+            <MenuItem key={provider.id} value={provider.id}>
+              {provider.firstName} {provider.lastName}
+            </MenuItem>
+          ))}
+        </Select>
+
+        <Button>Save</Button>
+      </FormControl>
+    </Box>
   );
 };
