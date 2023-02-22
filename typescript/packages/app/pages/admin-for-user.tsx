7b6a8543bc438d1587de6b882f605a8bffd7cf85
Nasr Maswood <nasr@songbirdcare.com>
2023-02-22 13:01:20 -0500

    start dummy admin for user page

---
+++ typescript/packages/app/pages/admin-for-user.tsx
@@ -0,0 +1,43 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
+import { Box, LinearProgress } from "@mui/material";
+import { useRouter } from "next/router";
+import * as React from "react";
+
+import { AdminForUser } from "../src/admin/admin-for-user";
+import { AppBar } from "../src/app-bar/app-bar";
+import { BodyContainer } from "../src/body-container";
+import { useFetchMe } from "../src/hooks/use-fetch-user";
+import { useFetchUsers } from "../src/hooks/use-fetch-users";
+
+const Admin: React.FC = () => {
+  const { data: user, isLoading: userIsLoading } = useFetchMe();
+
+  const { data: users } = useFetchUsers();
+
+  const router = useRouter();
+
+  const role = user?.role;
+  React.useEffect(() => {
+    if (role && role !== "admin") {
+      router.push("/");
+    }
+  }, [router, role]);
+
+  return (
+    <>
+      <AppBar />
+
+      <BodyContainer>
+        {userIsLoading || !user || !users ? (
+          <Box display="flex" width="100%" height="100%">
+            <LinearProgress sx={{ width: "100%" }} />
+          </Box>
+        ) : (
+          <AdminForUser />
+        )}
+      </BodyContainer>
+    </>
+  );
+};
+
+export default withPageAuthRequired(Admin);
